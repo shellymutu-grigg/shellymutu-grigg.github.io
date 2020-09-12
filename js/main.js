ce7ad8ec239103ad53265daa9fbdd5f457909416
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-09-12 21:03:47 +1200

    Update website for js navbar rendering

--- js/main.js
+++ js/main.js
@@ -1,31 +1,90 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
-// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+/* 
+# Wait for the DOM to load and call start()
+# https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+*/
 document.addEventListener('DOMContentLoaded', start)
 
- function start () {
-     for(var i = document.querySelectorAll.length; i > 0; i--){
-            console.log('document.getElementById("navbar"): '+ document.getElementById('navbar'))
-     }
-     var navbar = document.getElementById('navbar')
-     myFunction()
-   }
+/*
+# Initiate page
+*/
+function start () {
+    buildNavBar()
+}
+/*
+# Automatically build the navbar for each page
+*/
+function buildNavBar() {
+    if (document.getElementById('navbar_home_id')===null){
+        var html = [
+            '<ul>',
+                '<li><a href="../index.html">Home</a>',
+                '<li><a href="#"id="top">Technical Blogs</a>',
+                '<!-- First Tier Drop Down -->',
+                    '<ul>', 
+                        '<li><a href="sprint1-technical.html">Sprint 01 Technical</a></li>',
+                        '<li><a href="sprint2-technical.html">Sprint 02 Technical</a></li>',
+                        '<li><a href="sprint3-technical.html">Sprint 03 Technical</a></li>',
+                        '<li><a href="sprint4-technical.html">Sprint 04 Technical</a></li>',
+                    '</ul>',        
+                '</li>',
+                '<li><a href="#">Cultural Blogs</a>',
+                '<!-- First Tier Drop Down -->',
+                    '<ul>',
+                        '<li><a href="sprint1-cultural.html">Sprint 01 Cultural</a></li>',
+                        '<li><a href="sprint2-cultural.html">Sprint 02 Cultural</a></li>',
+                        '<li><a href="sprint3-cultural.html">Sprint 03 Cultural</a></li>',
+                        '<li><a href="sprint4-cultural.html">Sprint 04 Cultural</a>',
+                    '</li>',
+                '</ul>',
+                '</li>',
+                '<li><a href="#">Contact</a></li>',
+            '</ul>'
+        ].join("\n")
+        document.getElementById('navbar_id').innerHTML += html
+    }else{
+        var html = [
+                '<ul>',
+                    '<li><a href="index.html">Home</a>',
+                    '<li><a href="#"id="top">Technical Blogs</a>',
+                    '<!-- First Tier Drop Down -->',
+                        '<ul>', 
+                            '<li><a href="blog/sprint1-technical.html">Sprint 01 Technical</a></li>',
+                            '<li><a href="blog/sprint2-technical.html">Sprint 02 Technical</a></li>',
+                            '<li><a href="blog/sprint3-technical.html">Sprint 03 Technical</a></li>',
+                            '<li><a href="blog/sprint4-technical.html">Sprint 04 Technical</a></li>',
+                        '</ul>',        
+                    '</li>',
+                    '<li><a href="#">Cultural Blogs</a>',
+                    '<!-- First Tier Drop Down -->',
+                        '<ul>',
+                            '<li><a href="blog/sprint1-cultural.html">Sprint 01 Cultural</a></li>',
+                            '<li><a href="blog/sprint2-cultural.html">Sprint 02 Cultural</a></li>',
+                            '<li><a href="blog/sprint3-cultural.html">Sprint 03 Cultural</a></li>',
+                            '<li><a href="blog/sprint4-cultural.html">Sprint 04 Cultural</a>',
+                        '</li>',
+                    '</ul>',
+                    '</li>',
+                    '<li><a href="#">Contact</a></li>',
+                '</ul>'
+        ].join("\n")
+        document.getElementById('navbar_home_id').innerHTML += html
+    }
+}
+/*
+# Add an event listener to the window
+*/
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed')
+    console.log('Add window.addEventListener')
 })
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
     myFunction()
 }
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+/*
+# Shell of myFunction()
+*/
 function myFunction() {
-//     console.log('myFunction(navbar)'+ document.getElementById('navbar'))
-//     var sticky = document.getElementById('navbar').offsetTop;
-//     if (window.pageYOffset >= sticky) {
-//         document.getElementById('navbar').classList.add("sticky")
-//   } else {
-//     document.getElementById('navbar').classList.remove("sticky")
-//   }
+    console.log('myFunction()')
 }
 
